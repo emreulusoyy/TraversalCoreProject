39f7a1aec32c86288d22126be4badad9e337c9fc
ulusoydev <eulusoy139@gmail.com>
2023-01-28 21:53:50 +0300

    Proje dosyası ekle.

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAbout2Service
+    {
+        IAbout2Dal _about;
+
+        public AboutManager(IAbout2Dal about)
+        {
+            _about = about;
+        }
+
+        public void TAdd(About2 t)
+        {
+            _about.Insert(t);   
+        }
+
+        public void TDelete(About2 t)
+        {
+            _about.Delete(t);
+        }
+
+        public About2 TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About2> TGetList()
+        {
+            return _about.GetList();    
+        }
+
+        public void TUpdate(About2 t)
+        {
+            _about.Update(t);
+        }
+    }
+}
---
+++ TraversalCoreProject/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
